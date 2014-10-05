267a539400af6f5ca9228434a592804c5451b3ab
Alex <alex.agency@gmail.com>
2014-10-05 10:08:58 +0300

    update

--- halloween.ino
+++ halloween.ino
@@ -1,6 +1,5 @@
 #include <SPI.h>
 
-// Debug info
 #define DEBUG
 
 #define __spi_latch 9
@@ -18,6 +17,143 @@
 byte brightness_red[__leds_per_row][__rows]; 
 byte brightness_green[__leds_per_row][__rows];
 byte brightness_blue[__leds_per_row][__rows]; 
+
+uint8_t blinkIndex[] = { 1, 2, 3, 4, 3, 2, 1 }; // Blink bitmap sequence
+uint8_t blinkCountdown = 100; // Countdown to next blink (in frames)
+uint8_t gazeCountdown  =  50; // Countdown to next eye movement
+uint8_t gazeFrames     =  20; // Duration of eye movement (smaller = faster)
+uint8_t eyeOffset = 0;
+uint8_t pupilX = 3, pupilY = 3;   // Current pupil position
+uint8_t newX = 3, newY = 3;   // Next pupil position
+int8_t dX   = 0, dY   = 0;   // Distance from prior to new position
+
+static const uint8_t PROGMEM
+blinkImg[][8] = {    // Eye animation frames
+    // The NICE eye, both left and right
+    { B00111100,         // Fully open nice eye
+        B01111110,
+        B11111111,
+        B11111111,
+        B11111111,
+        B11111111,
+        B01111110,
+        B00111100 },
+    { B00000000,
+        B01111110,
+        B11111111,
+        B11111111,
+        B11111111,
+        B11111111,
+        B01111110,
+        B00111100 },
+    { B00000000,
+        B00000000,
+        B00111100,
+        B11111111,
+        B11111111,
+        B11111111,
+        B00111100,
+        B00000000 },
+    { B00000000,
+        B00000000,
+        B00000000,
+        B00111100,
+        B11111111,
+        B01111110,
+        B00011000,
+        B00000000 },
+    { B00000000,         // Fully closed nice eye
+        B00000000,
+        B00000000,
+        B00000000,
+        B10000001,
+        B01111110,
+        B00000000,
+        B00000000 },
+
+    // The EVIL eye, left
+    { B00000000,         // Fully open evil eye
+        B11000000,
+        B11111000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00111110,
+        B00011100 },
+    { B00000000,
+        B00000000,
+        B11111000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00111110,
+        B00011100 },
+    { B00000000,
+        B00000000,
+        B11111000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00111110,
+        B00000000 },
+    { B00000000,
+        B00000000,
+        B00000000,
+        B01111111,
+        B01111111,
+        B00111111,
+        B00000000,
+        B00000000 },
+    { B00000000,         // Fully closed evil eye
+        B00000000,
+        B00000000,
+        B00000000,
+        B01111111,
+        B00000000,
+        B00000000,
+        B00000000 },
+    // The EVIL eye, right
+    { B00000000,         // Fully open evil eye
+        B00000011,
+        B00011111,
+        B11111110,
+        B11111110,
+        B11111100,
+        B01111100,
+        B00111000 },
+    { B00000000,
+        B00000000,
+        B00011111,
+        B11111110,
+        B11111110,
+        B11111100,
+        B01111100,
+        B00111000 },
+    { B00000000,
+        B00000000,
+        B00011111,
+        B11111110,
+        B11111110,
+        B11111100,
+        B01111100,
+        B00000000 },
+    { B00000000,
+        B00000000,
+        B00000000,
+        B11111110,
+        B11111110,
+        B11111100,
+        B00000000,
+        B00000000 },
+    { B00000000,         // Fully closed evil eye
+        B00000000,
+        B00000000,
+        B00000000,
+        B11111110,
+        B00000000,
+        B00000000,
+        B00000000 }
+};
 
 // Declare output
 static int serial_putchar(char c, FILE *) {
@@ -38,7 +174,7 @@ void setup() {
 
   set_matrix_rgb(0,0,0);
   setup_timer1_ovf();
-  matrix_test();
+  matrix_test(10);
 }
 
 ISR(TIMER1_OVF_vect) {
@@ -79,57 +215,63 @@ ISR(TIMER1_OVF_vect) {
   }
 }
 
-void loop() {
-  set_row_byte_blue(0, B00111100, __max_brightness);
-  set_row_byte_blue(1, B01111110, __max_brightness);
-  set_row_byte_blue(2, B11111111, __max_brightness);
-  set_row_byte_blue(3, B11110011, __max_brightness);
-  set_row_byte_blue(4, B11110011, __max_brightness);
-  set_row_byte_blue(5, B11111111, __max_brightness);
-  set_row_byte_blue(6, B01111110, __max_brightness);
-  set_row_byte_blue(7, B00111100, __max_brightness);
-  delay(500);
-  set_row_byte_blue(3, B11111111, __max_brightness);
-  set_row_byte_blue(4, B11100111, __max_brightness);
-  set_row_byte_blue(5, B11100111, __max_brightness);
-  delay(30);
-  set_row_byte_blue(4, B11111111, __max_brightness);
-  set_row_byte_blue(5, B11001111, __max_brightness);
-  set_row_byte_blue(6, B01001110, __max_brightness);
-  delay(500);
+void loop() 
+{
+  const uint8_t* eye = 
+            &blinkImg[
+            (blinkCountdown < sizeof(blinkIndex)) ? // Currently blinking?
+            blinkIndex[blinkCountdown] :            // Yes, look up bitmap #
+            0                                       // No, show bitmap 0
+            ][0] + eyeOffset; // 8*5
+  
+  blinkCountdown--;
+    if (blinkCountdown == 0) 
+        blinkCountdown = random(5, 180);
 
-  set_row_byte_blue(0, B00000000, __max_brightness);
+  if(--gazeCountdown <= gazeFrames) {
+    // pupil in motion - draw pupil at interim position
+    drawEyes(eye, 
+      newX - (dX * gazeCountdown / gazeFrames),
+      newY - (dY * gazeCountdown / gazeFrames));
+    
+    if(gazeCountdown == 0) {    // Last frame?
+      pupilX = newX; pupilY = newY;
+      
+      // Pick random positions
+      newX = random(1,6); newY = random(2,5);
+      
+      dX            = newX - pupilX;           // Horizontal distance to move
+      dY            = newY - pupilY;           // Vertical distance to move
+      gazeFrames    = random(3, 15);           // Duration of pupil movement
+      gazeCountdown = random(gazeFrames, 120); // Count to end of next movement
+    }
+  } else {
+      // Not in motion yet -- draw pupil at current static position
+      drawEyes(eye, pupilX, pupilY);
+  }
+}
+
+void drawEyes(const uint8_t* eye, uint8_t pupilX, uint8_t pupilY) {
+  for(byte ctr1 = 0; ctr1 <= __max_row; ctr1++) {
+      byte image = bit_reverse(pgm_read_byte_near(eye+ctr1));
+      
+      if(ctr1==pupilY || ctr1==pupilY+1) {
+        image = image-(3<<pupilX);
+      }
+      
+      set_row_byte_blue(ctr1,image,__max_brightness);
+  }
+  #ifdef DEBUG
+    //printf_P(PSTR("pupil: x=%d, y=%d\n\r"), pupilX, pupilY);
+  #endif
   delay(5);
-  set_row_byte_blue(1, B00000000, __max_brightness);
-  set_row_byte_blue(2, B00111100, __max_brightness);
-  set_row_byte_blue(4, B11111111, __max_brightness);
-  set_row_byte_blue(5, B11111111, __max_brightness);
-  set_row_byte_blue(6, B00111100, __max_brightness);
-  set_row_byte_blue(7, B00000000, __max_brightness);
-  delay(5);
-  set_row_byte_blue(2, B00000000, __max_brightness);
-  set_row_byte_blue(3, B00111100, __max_brightness);
-  set_row_byte_blue(5, B01111110, __max_brightness);
-  set_row_byte_blue(6, B00011000, __max_brightness);
-  delay(5);
-  set_row_byte_blue(3, B00000000, __max_brightness);
-  set_row_byte_blue(4, B10000001, __max_brightness);
-  set_row_byte_blue(6, B00000000, __max_brightness);
-  delay(5);
-  set_row_byte_blue(3, B00111100, __max_brightness);
-  set_row_byte_blue(4, B11111111, __max_brightness);  
-  set_row_byte_blue(6, B00011000, __max_brightness);
-  delay(5);
-  set_row_byte_blue(2, B00111100, __max_brightness);
-  set_row_byte_blue(3, B11111111, __max_brightness);
-  set_row_byte_blue(5, B11111111, __max_brightness);
-  set_row_byte_blue(6, B00111100, __max_brightness);
-  delay(5);
-  set_row_byte_blue(0, B00111100, __max_brightness);
-  set_row_byte_blue(1, B01111110, __max_brightness);
-  set_row_byte_blue(2, B11111111, __max_brightness);
-  set_row_byte_blue(6, B01111110, __max_brightness);
-  set_row_byte_blue(7, B00111100, __max_brightness);
+}
+
+byte bit_reverse( byte x ) { 
+  x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa); 
+  x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc); 
+  x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0); 
+  return x;    
 }
 
 void setup_timer1_ovf() {
@@ -204,15 +346,39 @@ void set_row_byte_rgb(byte row, byte data_byte, byte red, byte green, byte blue)
 }
 
 void set_row_byte_red(byte row, byte data_byte, byte red) {
-  set_row_byte_rgb(row,data_byte,red,0,0);
+  byte led;
+  for(led = 0; led <= __max_led; led++) {
+    if( (data_byte>>led)&(B00000001) ) {
+      set_led_red(row,led,red);
+    }
+    else {
+      set_led_red(row,led,0);
+    }
+  }
 }
 
 void set_row_byte_green(byte row, byte data_byte, byte green) {
-  set_row_byte_rgb(row,data_byte,0,green,0);
+  byte led;
+  for(led = 0; led <= __max_led; led++) {
+    if( (data_byte>>led)&(B00000001) ) {
+      set_led_green(row,led,green);
+    }
+    else {
+      set_led_green(row,led,0);
+    }
+  }
 }
 
 void set_row_byte_blue(byte row, byte data_byte, byte blue) {
-  set_row_byte_rgb(row,data_byte,0,0,blue);
+  byte led;
+  for(led = 0; led <= __max_led; led++) {
+    if( (data_byte>>led)&(B00000001) ) {
+      set_led_blue(row,led,blue);
+    }
+    else {
+      set_led_blue(row,led,0);
+    }
+  }
 }
 
 void set_matrix_rgb(byte red, byte green, byte blue) {
@@ -225,42 +391,43 @@ void set_matrix_rgb(byte red, byte green, byte blue) {
   }
 }
 
-void matrix_test() {
+void matrix_test(int speed) {
   byte ctr1;
   byte ctr2;
   for(ctr1 = 0; ctr1 <= __max_brightness; ctr1++) {
     set_matrix_rgb(ctr1,0,0);
-    delay(3);
+    delay(speed/10);
   }
   for(ctr1 = 0; ctr1 <= __max_brightness; ctr1++) {
     set_matrix_rgb(0,ctr1,0);
-    delay(3);
+    delay(speed/10);
   }
   for(ctr1 = 0; ctr1 <= __max_brightness; ctr1++) {
     set_matrix_rgb(0,0,ctr1);
-    delay(3);
+    delay(speed/10);
   }
   set_matrix_rgb(0,0,0);
   
   for(ctr1 = 0; ctr1 <= __max_row; ctr1++) {
     set_row_rgb(ctr1,__max_brightness,0,0);
-    delay(50);
+    delay(speed);
     set_row_rgb(ctr1,0,__max_brightness,0);
-    delay(50);
+    delay(speed);
     set_row_rgb(ctr1,0,0,__max_brightness);
-    delay(50);
+    delay(speed);
     set_matrix_rgb(0,0,0);
   }
 
   for(ctr1 = 0; ctr1 <= __max_row; ctr1++) {
     set_column_rgb(ctr1,__max_brightness,0,0);
-    delay(50);
+    delay(speed);
     set_column_rgb(ctr1,0,__max_brightness,0);
-    delay(50);
+    delay(speed);
     set_column_rgb(ctr1,0,0,__max_brightness);
-    delay(50);
+    delay(speed);
     set_matrix_rgb(0,0,0);
   }
-
-  delay(1000);
+  set_row_byte_rgb(0,1,__max_brightness,__max_brightness,__max_brightness);
+  delay(speed*50);
+  set_matrix_rgb(0,0,0);
 }
